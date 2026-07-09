2628859b788731e266a98e16c18cd48d88ec8092
Devika-sajeev1010 <devikadiya1010@gmail.com>
2026-07-09 22:45:50 +0530

    solved Two-sumII-Input-array-is-sorted

---
+++ 0167-Two-sumII-Input-array-is-sorted/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l = 0
+        r = len(numbers)-1
+        ans=[]
+        while l < r:
+            if numbers[l]+numbers[r]>target:
+                r-=1
+            elif numbers[l]+numbers[r] < target:
+                l+=1
+            else:
+                ans.append(l+1)
+                ans.append(r+1)
+                break
+        return ans
+
+
+
+        
